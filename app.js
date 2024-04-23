1ed898b21d1ee6122bad7c8e49fc2a3c39a63965
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-04-23 20:38:40 +0500

    Prayer Timing App Uncomplete

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const city_search = document.getElementById("city-search");
 const submit_city = document.getElementById("submit-city");
 const city_name = document.getElementById("city-name");
 const fajar_time = document.getElementById("fajar-time");
-const sunrise_time = document.getElementById("submit-city");
-const dhuhr_time = document.getElementById("submit-city");
-const asr_time = document.getElementById("submit-city");
-const maghrib_time = document.getElementById("submit-city");
-const isha_time = document.getElementById("submit-city");
+const sunrise_time = document.getElementById("sunrise-time");
+const dhuhr_time = document.getElementById("dhuhr-time");
+const asr_time = document.getElementById("asr-time");
+const maghrib_time = document.getElementById("maghrib-time");
+const isha_time = document.getElementById("isha-time");
 
 let currentDate = new Date();
 
-async function checkName(city) {
+async function checkName(city_search) {
   const url = `https://api.aladhan.com/v1/timingsByAddress/${currentDate}?address=${city_search}`;
-  const city_data = await fetch(url).then((response) => response.json());
+  const city_data = await fetch(`${url}`).then((response) => response.json());
+  console.log (city_data);
 }
 
 submit_city.addEventListener(`click`, () => {
